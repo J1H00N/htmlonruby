3255a61c15007f76003da736dd34898921a55383
J1H00N <hoonie524@gmail.com>
2017-04-05 14:17:59 +0900

    success!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,23 +1,20 @@
 class HomeController < ApplicationController
 	def aboutme
-		model = {
-			name: "김지훈",
-  			age: "24",
-			education: "건국대학교",
-			major: "경영학과",
-			phone: "010123456789",
-			email: "hoonie524@gmail.com",
-			address: "화양동 건국대학교 경영대학 107-1",
-			description: "경영대 쿨가이"
-			}
-
-			@HTMLName = model[:name]
-			@HTMLAge = model[:age]
-			@HTMLEducation = model[:education],
-			@HTMLMajor = model[:major],
-			@HTMLPhone = model[:phone],
-			@HTMLEmail = model[:email],
-			@HTMLAddress = model[:address],
-			@HTMLDescription = model[:description];
+			myname = "김지훈",
+  			age = "24",
+			education = "건국대학교",
+			major = "경영학과",
+			phone = "010123456789",
+			email = "hoonie524@gmail.com",
+			address = "화양동 건국대학교 경영대학 107-1",
+			description = "경영대 쿨가이",
+			@HTMLName = myname,
+			@HTMLAge = age,
+			@HTMLEducation = education,
+			@HTMLMajor = major,
+			@HTMLPhone = phone,
+			@HTMLEmail = email,
+			@HTMLAddress = address,
+			@HTMLDescription = description;
 	end
 end
