c3b57c2451cc0f5c0c02fb7eebb759b232242a6d
J1H00N <hoonie524@gmail.com>
2017-04-05 13:51:52 +0900

    unexpected error occured

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,13 +10,14 @@ class HomeController < ApplicationController
 			address: "화양동 건국대학교 경영대학 107-1",
 			description: "경영대 쿨가이"
 			}
-			HTMLName = model[:name]
-			HTMLAge = model[:age]
-			HTMLEducation = model[:education],
-			HTMLMajor = model[:major],
-			HTMLPhone = model[:phone],
-			HTMLEmail = model[:email],
-			HTMLAddress = model[:address],
-			HTMLDescription = model[:description];
+
+			@HTMLName = model[:name]
+			@HTMLAge = model[:age]
+			@HTMLEducation = model[:education],
+			@HTMLMajor = model[:major],
+			@HTMLPhone = model[:phone],
+			@HTMLEmail = model[:email],
+			@HTMLAddress = model[:address],
+			@HTMLDescription = model[:description];
 	end
 end
