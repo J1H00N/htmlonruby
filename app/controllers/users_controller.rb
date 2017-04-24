3f96657baf9196c92d04c45a651412345b6bde7e
J1H00N <hoonie524@gmail.com>
2017-04-25 05:57:55 +0900

    3분기 휴학생 과제 완료

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+	def index
+			myname = "김지훈",
+  			age = "24",
+			education = "건국대학교",
+			major = "경영학과",
+			phone = "010123456789",
+			email = "hoonie524@gmail.com",
+			address = "화양동 건국대학교 경영대학 107-1",
+			description = "경영대 쿨가이",
+			@HTMLName = myname,
+			@HTMLAge = age,
+			@HTMLEducation = education,
+			@HTMLMajor = major,
+			@HTMLPhone = phone,
+			@HTMLEmail = email,
+			@HTMLAddress = address,
+			@HTMLDescription = description;
+			@user = User.find(1)
+	end
+
+	def show
+		@user = User.find(1)
+	end
+
+	private
+  	def user_params
+    params.require(:user).permit(
+      :phone,
+      :email
+    )
+  	end
+end
